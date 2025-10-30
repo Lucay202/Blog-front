0b2e22e4a9e1fd3dd74719dcd8f4a32574e4c5d6
Lucay202 <Lucay202@gmail.com>
2025-10-30 22:39:39 +0000

    Fix WORKER_BASE URL by removing duplicate 'https://'

--- scripts/blog.js
+++ scripts/blog.js
@@ -1,6 +1,6 @@
 // scripts/blog.js
 // Replace WORKER_BASE with your worker URL (e.g. 'https://my-worker.workers.dev')
-const WORKER_BASE = 'https://https://blog.lucay202.workers.dev/';
+const WORKER_BASE = 'https://blog.lucay202.workers.dev';
 
 function getSlugFromPath() {
   const parts = location.pathname.split('/').filter(Boolean);
